5abe6f9ec6c9406d44d2c1f6a1a6fbddecc1c9f7
ryun <run2070@naver.com>
2022-03-02 18:40:14 +0900

    initial commit

--- app.js
+++ app.js
@@ -5,14 +5,28 @@ const loginForm = document.querySelector("#login-form");
 
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("#login-form a")
+const h1Text = document.querySelector("#greeting");
+
 function onLoginSubmit(info){
-    // info.preventDefault();
-    // console.log(info);
+    info.preventDefault();
     const userName = loginInput.value;
-    console.log(userName);
+    loginForm.classList.add("hidden");
+    h1Text.classList.remove("hidden");
+    // h1Text.innerText = "hello welcome "+userName
+    h1Text.innerText = `hello welcome ${userName}`
+}
+
+function handleListClick(event){
+
+    console.log(event);
+    loginForm.className
+    alert("test");
 
 }
 
+
+link.addEventListener("click", handleListClick)
 loginForm.addEventListener("submit", onLoginSubmit)
 
 
