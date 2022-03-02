b25ef586801168a6de4c3e3cff85d331df469855
ryun <run2070@naver.com>
2022-03-02 19:27:02 +0900

    userName localStorage 저장로직 추가

--- app.js
+++ app.js
@@ -8,13 +8,31 @@ const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("#login-form a")
 const h1Text = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden"
+
+function initialLogic(){
+
+    const storageUserName = localStorage.getItem("userName");
+
+    if(storageUserName!=null){
+        loginForm.classList.add(HIDDEN_CLASSNAME);
+        h1Text.classList.remove(HIDDEN_CLASSNAME);
+
+        h1Text.innerText = `hello welcome ${storageUserName}`;
+    }
+
+}
+
+initialLogic();
+
 function onLoginSubmit(info){
     info.preventDefault();
     const userName = loginInput.value;
-    loginForm.classList.add("hidden");
-    h1Text.classList.remove("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    h1Text.classList.remove(HIDDEN_CLASSNAME);
     // h1Text.innerText = "hello welcome "+userName
-    h1Text.innerText = `hello welcome ${userName}`
+    h1Text.innerText = `hello welcome ${userName}`;
+    localStorage.setItem("userName", userName);
 }
 
 function handleListClick(event){
